b9d92bc69f20aa2b9b419fb3a6d1bff89ec6d626
ekkkk1 <asdqwek200@naver.com>
2021-04-10 00:02:33 +0900

    map.forEach

---
+++ src/main/java/me/gicheol/lambdastream/lambda/MapStream.java
@@ -0,0 +1,17 @@
+package me.gicheol.lambdastream.lambda;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapStream {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "1");
+        map.put("2", "2");
+        map.put("3", "3");
+        map.put("4", "4");
+        map.put("5", "5");
+
+        map.forEach((k, v) -> System.out.println("{" + k + ", " + k + "}"));
+    }
+}
