bba4482d3b2174c69ac87a094cd50dff2685dc62
ekkkk1 <asdqwek200@naver.com>
2021-04-09 23:57:15 +0900

    Predicate 사용법

---
+++ src/test/java/me/gicheol/lambdastream/LambdaStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gicheol.lambdastream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
